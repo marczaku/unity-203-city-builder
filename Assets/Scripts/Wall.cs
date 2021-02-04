356505111bff4f8a8479f4330cb3bf09c2f87275
Marc Zaku <mzaku@hotmail.de>
2021-02-04 14:22:54 +0100

    updates wall script with collision detectors with small faults

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -1,18 +1,25 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Wall : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+public class Wall : MonoBehaviour, IDragHandler{
+    
+    public List<Vector3> hitPoints = new List<Vector3>();
+    public void OnDrag(PointerEventData eventData){
+        // Get the world position of the pointer:
+        // It will be on the Camera's position
+        var position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // Remember that point, so we can draw gizmos in the scene view for debugging:
+        this.hitPoints.Add(position);
+        // Calculate the local position for this world point on the Isometric Grid:
+        position = this.transform.parent.InverseTransformPoint(position);
+        // Set the wall's local position to the floored, calculated local position:
+        this.transform.localPosition = new Vector3(Mathf.Floor(position.x), Mathf.Floor(position.y), position.z);
         
+        // Problem: The tile is now stuck hovering over the Isometric Grid, stuck to the Camera Screen.
+        // Fix: Use RayCasts in order to calculate the correct z position for the Mouse Input
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void OnDrawGizmos() {
+        this.hitPoints.ForEach(point => Gizmos.DrawCube(point, Vector3.one * 0.5f));
     }
 }
